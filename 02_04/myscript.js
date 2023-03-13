98a90cd05d72582bf4f8fb9301072e20f5383b2a
devops-sagar <pathaksagar16@gmail.com>
2023-03-13 16:47:06 -0700

    worked with objects

--- 02_04/myscript.js
+++ 02_04/myscript.js
@@ -1,6 +1,7 @@
 import biodata from "./biodata.js";
+import skills from "./skills.js";
 
-const markup = (biodata) => {
+const markup = (biodata, skills) => {
     return `
     <div>
         <h2>Welcome ${biodata.name}!</h2>
@@ -11,11 +12,21 @@ const markup = (biodata) => {
             <li>Position: ${biodata.position} </li>
             <li>Looking for Job? ${biodata.lookingjob ? "Yes" : "No"} </li>
             <li>Blood Group: ${biodata.bloodgroup.letter}${biodata.bloodgroup.sign} </li>
+            <li>Programming Languages: ${skills.programmingLanguage.p2} </li>
         </ul>
     </div>
     `;
 };
 
 const main = document.createElement("main");
-main.innerHTML = markup(biodata);
+main.innerHTML = markup(biodata,skills);
 document.body.appendChild(main);
+
+console.log("This one is the blodgroup sign: ", biodata.bloodgroup.sign)
+
+var random = "bloodgroup"
+var randomAgain = "sign"
+console.log("To demonstrate we can use also as variable:",biodata[random][randomAgain])
+console.log("To demonstrate we can use also as variable:",biodata["color"])
+
+console.log(skills.programmingLanguage)
---
+++ 02_04/skills.js
@@ -0,0 +1,20 @@
+const skills = {
+
+    spokenLanguage: {
+        l1: "English",
+        l2: "Hindi",
+        l3: "Gujarati",
+        l4: "Sanskrit",
+    },
+
+    programmingLanguage: {
+        p1: "Python",
+        p2: "JavaScript",
+        p3: "React.js",
+        p4: "C#"
+    },
+
+    communication: "High",
+};
+
+export default skills;
