bb23776018b70c909ab07a5fac0e1ef698c08d5a
devops-sagar <pathaksagar16@gmail.com>
2023-03-15 20:07:09 -0700

    fixing bug on not rendering the UI with usage of clas

--- 03_10/myscript.js
+++ 03_10/myscript.js
@@ -10,32 +10,32 @@ const obj = new biodata(
     "+"
 );
 
-console.log(obj.sign);
+console.log("This one is the blodgroup sign: ", obj.sign)
 
-const markup = (biodata) => {
-    return `
-    <div>
-        <h2>Welcome ${obj.name}!</h2>
-        <h4>profile Information</h4>
-        <ul>
-            <li>Height: ${obj.height} </li>
-            <li>Color: ${obj.color} </li>
-            <li>Position: ${obj.position} </li>
-            <li>Looking for Job? ${obj.lookingjob ? "Yes" : "No"} </li>
-            <li>Blood Group: ${obj.bloodgroup.letter}${obj.bloodgroup.sign} </li>
-        </ul>
-    </div>
-    `;
-};
-
-const main = document.createElement("main");
-main.innerHTML = markup(biodata);
-document.body.appendChild(main);
-
-// console.log("This one is the blodgroup sign: ", obj.bloodgroup.sign)
-
+console.log("To demonstrate we can use also as variable:",obj["color"])
 // var random = "bloodgroup"
-// var randomAgain = "sign"
-// console.log("To demonstrate we can use also as variable:",obj[random][randomAgain])
-// console.log("To demonstrate we can use also as variable:",obj["color"])
+var random = "letter"
+var randomAgain = "sign"
+console.log("To demonstrate we can use also as variable:",obj[random]+obj[randomAgain])
+
+// const markup = (biodata) => {
+//     return `
+//     <div>
+//         <h2>Welcome ${obj.name}!</h2>
+//         <h4>profile Information</h4>
+//         <ul>
+//             <li>Height: ${obj.height} </li>
+//             <li>Color: ${obj.color} </li>
+//             <li>Position: ${obj.position} </li>
+//             <li>Looking for Job? ${obj.lookingjob ? "Yes" : "No"} </li>
+//             <li>Blood Group: ${obj.bloodgroup.letter}${obj.bloodgroup.sign} </li>
+//         </ul>
+//     </div>
+//     `;
+// };
+
+// const main = document.createElement("main");
+// main.innerHTML = markup(biodata);
+// document.body.appendChild(main);
+
 
